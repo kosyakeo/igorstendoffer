295925f8fec7bb07a88f33b63b5da2815c71ebbb
kosyakeo <142715653+kosyakeo@users.noreply.github.com>
2025-05-23 15:30:54 +0700

    3.py

--- Учебная практика 2/2.1/3.py
+++ Учебная практика 2/2.1/3.py
@@ -1,10 +1,9 @@
-def contains_duplicate(nums):
-    seen = set()
-    for num in nums:
-        if num in seen:
-            return True
-        seen.add(num)
+def contains(nums):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums [i] == nums[j]:
+                return True
     return False
-print(contains_duplicate([1, 2, 3, 4])) #Ложььььь
-print(contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])) #Правдаааа
-print(contains_duplicate([1, 2, 3, 1])) #Правдааааааааааа
+print(contains([1, 2, 3, 4]))
+print(contains([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+print(contains([1, 2, 3, 1]))
